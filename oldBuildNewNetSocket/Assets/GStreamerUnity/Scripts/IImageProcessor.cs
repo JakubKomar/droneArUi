66fb5b2b8f4cac9b09de046c265c0dc054fc4a40
komca12 <komca12@gmail.com>
2023-12-13 17:26:34 +0100

    final init

--- oldBuildNewNetSocket/Assets/GStreamerUnity/Scripts/IImageProcessor.cs
+++
@@ -1,7 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-
-public abstract class IImageProcessor : MonoBehaviour {
-	
-	public abstract Texture ProcessTexture (Texture InputTexture, ref RenderTexture ResultTexture, int downSample = 0);
-}
