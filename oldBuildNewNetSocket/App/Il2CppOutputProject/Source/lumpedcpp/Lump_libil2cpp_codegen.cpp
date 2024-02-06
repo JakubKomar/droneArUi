cac44e5a250a5be337eb43cfe7129e9b615242ea
komca12 <komca12@gmail.com>
2024-02-06 15:54:22 +0100

    oldSocketDeletion

--- oldBuildNewNetSocket/App/Il2CppOutputProject/Source/lumpedcpp/Lump_libil2cpp_codegen.cpp
+++
@@ -1,3 +0,0 @@
-#include "il2cpp-config.h"
-#include "C:\Users\Marek\Desktop\build_Alpha\Il2CppOutputProject\IL2CPP\libil2cpp\codegen\il2cpp-codegen-common.cpp"
-#include "C:\Users\Marek\Desktop\build_Alpha\Il2CppOutputProject\IL2CPP\libil2cpp\codegen\il2cpp-codegen-il2cpp.cpp"
--- oldBuildNewNetSocket/App/Il2CppOutputProject/Source/lumpedcpp/Lump_libil2cpp_debugger.cpp
+++
@@ -1,2 +0,0 @@
-#include "il2cpp-config.h"
-#include "C:\Users\Marek\Desktop\build_Alpha\Il2CppOutputProject\IL2CPP\libil2cpp\debugger\il2cpp-stubs.cpp"
