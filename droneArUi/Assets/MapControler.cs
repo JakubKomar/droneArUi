c8302c6ecf01f9077ceab59a8fd3ab85c9a2c928
komca12 <komca12@gmail.com>
2024-02-20 17:04:21 +0100

    map moving done

--- droneArUi/Assets/MapControler.cs
+++ droneArUi/Assets/MapControler.cs
@@ -14,43 +14,71 @@ public class MapControler : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        initCenter = abstractMap.CenterLatitudeLongitude;
         //mapInitY = mapTransform.position.y;
     }
 
     // Update is called once per frame
     void Update()
     {
-
-        float val = 19f- zoom;
-
-        float scaleIndex = val;
-
-        float newY = -0.434f;
-        if (val < 1 && val > -1) { 
-
-
-        }
-
-        else 
-        {
-            newY = newY / math.pow(2, scaleIndex);
-        }
  
-        mapTransform.localPosition = new Vector3(0, -0.44f * mapTransform.localScale.y, 0);
+        mapTransform.localPosition = new Vector3(0, mapInitY * mapTransform.localScale.y, 0);
   
     }
+
+    private Mapbox.Utils.Vector2d initCenter;
     public AbstractMap abstractMap = null;
     public PinchSlider pinchSlider = null;
     public Transform mapTransform = null;
-    private float mapInitY = -0.44f;
+    private float mapInitY = -0.51f;
     private float sliderVal = 0.5f;
     private float zoom = 19f;
+
+    private float moveStepSize = 0.0005f;
 
     public void setZoom()
     {
         sliderVal = pinchSlider.SliderValue;
-        zoom = 19f + math.round((sliderVal * 6f) - 3f);
+        zoom = math.round(19f + sliderVal * 4f - 2f);
 
         abstractMap.UpdateMap(zoom);
+    }
+
+    private float getStepSize() {
+        return moveStepSize / mapTransform.localScale.x;
+    }
+
+    public void moveUp()
+    {
+        Mapbox.Utils.Vector2d actualCenter = abstractMap.CenterLatitudeLongitude;
+        actualCenter.y = actualCenter.y - getStepSize();
+
+        abstractMap.UpdateMap(actualCenter, zoom);
+    }
+
+    public void moveDown()
+    {
+        Mapbox.Utils.Vector2d actualCenter = abstractMap.CenterLatitudeLongitude;
+        actualCenter.y = actualCenter.y + getStepSize();
+
+        abstractMap.UpdateMap(actualCenter, zoom);
+    }
+    public void moveLeft()
+    {
+        Mapbox.Utils.Vector2d actualCenter = abstractMap.CenterLatitudeLongitude;
+        actualCenter.x = actualCenter.x + getStepSize();
+
+        abstractMap.UpdateMap(actualCenter, zoom);
+    }
+    public void moveRight()
+    {
+        Mapbox.Utils.Vector2d actualCenter = abstractMap.CenterLatitudeLongitude;
+        actualCenter.x = actualCenter.x - getStepSize();
+
+        abstractMap.UpdateMap(actualCenter, zoom);
+    }
+    public void center()
+    {
+        abstractMap.UpdateMap(initCenter, zoom);
     }
 }
