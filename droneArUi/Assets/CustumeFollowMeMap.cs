afd70f5c79509ed26fbd44ddef4376eaf6fb881e
komca12 <komca12@gmail.com>
2024-02-21 16:58:12 +0100

    minimap auto fallow feature

---
+++ droneArUi/Assets/CustumeFollowMeMap.cs
@@ -0,0 +1,36 @@
+// author jakub komárek
+
+using Microsoft.MixedReality.Toolkit.UI;
+using Microsoft.MixedReality.Toolkit.Utilities.Solvers;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CustumeFollowMeMap :  RadialView
+{
+
+    // Update is called once per frame
+    public Transform transformMap=null;
+    public Transform mainCameraTransform = null;
+    public FollowMeToggle followMeTogle = null;
+    void Update()
+    {
+
+        Vector3 goalPosition = base.GoalPosition;
+        Quaternion goalRotation = base.GoalRotation;
+
+        transformMap.position = goalPosition;
+
+        goalRotation.x = 0;
+        //goalRotation.y = (float)(Math.Round(goalRotation.y / 0.2f)) *0.2f;
+        goalRotation.z = 0;
+        transformMap.rotation = goalRotation;
+
+    }
+    private bool anchored = false;
+    public void onAchorClick() {
+        followMeTogle.ToggleFollowMeBehavior();
+        anchored = !anchored;
+    }
+}
