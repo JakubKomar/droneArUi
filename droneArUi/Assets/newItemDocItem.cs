a45b804197ad383a3de83c711d9fcbcfa716d1cd
komca12 <komca12@gmail.com>
2024-03-21 11:37:09 +0100

    icon update

--- droneArUi/Assets/newItemDocItem.cs
+++ droneArUi/Assets/newItemDocItem.cs
@@ -1,3 +1,5 @@
+// autor: jakub komárek
+using Microsoft.MixedReality.Toolkit.UI;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
@@ -15,24 +17,62 @@ public class newItemDocItem : MonoBehaviour
     [SerializeField]
     public MapObject.ObjType type;
 
+    private GameObject movebleGameObject=null;
+
     [SerializeField]
     public string description="";
     void Start()
     {
+        // nastavení popisku
         descriptionTextMash.text = description;
 
-        // spawn prefab
+        // vytvoø umístitelný prefab
+        if (itemPrefab != null)
+        {
+            movebleGameObject = Instantiate(itemPrefab);
+            movebleGameObject.transform.parent = this.transform;
+            movebleGameObject.transform.localScale = Vector3.one;
+            movebleGameObject.transform.localPosition = Vector3.zero;
+        }
+        else
+        {
+            Debug.LogError("spawner: " + description + " dont have prefab");
+            return;
+        }
 
         // add liseners for grap and release
+        ObjectManipulator manipulator = movebleGameObject.GetComponent<ObjectManipulator>();
+
+        if (manipulator != null)
+        {
+            manipulator.OnManipulationStarted.AddListener(onManipulationStart);
+            manipulator.OnManipulationEnded.AddListener(onManipulationEnd);
+        }
+        else
+        {
+            Debug.LogError("prefab in spawner: " + description + " dont have MRTK manipulator script");
+            return;
+        }
     }
 
-    // Update is called once per frame
+    private void onManipulationStart(ManipulationEventData eventData)
+    {
+        // nepotøebuju
+    }
+
+
+    private void onManipulationEnd(ManipulationEventData eventData)
+    {
+        // call shared data create
+        // return object to spawner
+        Debug.Log("create object metod called");
+
+        movebleGameObject.transform.localScale = Vector3.one;
+        movebleGameObject.transform.localPosition = Vector3.zero;
+    }
+
+
     void Update()
     {
-        // if object graped - object handover to spawnOnMap
-
-        // start 500s timer
     }
-
-    // after 500s create new itemPrefab
 }
