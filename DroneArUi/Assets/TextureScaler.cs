22feca4f5b3afa3b416f519836e11c1be6673a96
komca12 <komca12@gmail.com>
2024-04-05 16:51:10 +0200

    textury

---
+++ DroneArUi/Assets/TextureScaler.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextureScaler : MonoBehaviour
+{
+    // Update is called once per frame
+    [SerializeField]
+    GameObject obj;
+    void Update()
+    {
+        Renderer objectRenderer = obj.GetComponent<Renderer>();
+        if (objectRenderer != null)
+        {
+            Vector3 objectScale = transform.localScale;
+            Material material = objectRenderer.material;
+
+            // Vypoèítání nového mìøítka textury na základì mìøítka objektu
+            Vector2 newScale = new Vector2(objectScale.x*0.1f, objectScale.y*0.1f);
+
+            // Nastavení nového mìøítka textury
+            material.mainTextureScale = newScale;
+        }
+        else
+        {
+            Debug.LogError("Tento objekt neobsahuje renderer.");
+        }
+    }
+}
